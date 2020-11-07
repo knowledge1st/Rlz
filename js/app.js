b7b73ad357d792dee56e17e2f696c3e4d35f031c
Nacho <nachon49@gmail.com>
2020-11-07 20:07:11 -0300

    commit5

--- js/app.js
+++ js/app.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
   });
 
 $("#login").click(()=>{
-    login
+    let url_auth= 'https://410iwdz3t5.execute-api.us-east-2.amazonaws.com/develop/authx'
+    $.post(url_auth).then(()=>{
+        alert(data);
+    });
   });
