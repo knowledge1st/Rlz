50c2d2e352a6106fafcdbed4825e46ce24466588
Nacho <nachon49@gmail.com>
2020-11-07 23:41:43 -0300

    c9

--- js/app.js
+++ js/app.js
@@ -4,11 +4,9 @@ $(document).ready(function(){
 
 $("#login").click(()=>{
     let url_auth= 'https://410iwdz3t5.execute-api.us-east-2.amazonaws.com/develop/authx'
-    $.ajax(url_auth,{
+    $.ajax({
+        url:url_auth,
+        type:'POST',
         contentType: 'application/json',
-        data: JSON.stringify({ "username": $("#username").val(), "password": $("#password").val()}),
-        dataType: 'json'})
-        .then((data)=>{
-        alert(data);
-    });
+        data: JSON.stringify({ "username": $("#username").val(), "password": $("#password").val()})})
   });
