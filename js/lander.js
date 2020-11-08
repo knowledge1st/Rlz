a35bedd4662779a8bf8df39652853e89448b53d4
Nacho <nachon49@gmail.com>
2020-11-08 00:20:19 -0300

    c15

--- js/lander.js
+++ js/lander.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     let _user=Cookies.get('X-User')
     if (!_token){
         window.location.href = 'https://knowledge1st.github.io/Rlz/index.html';
+        Cookies.remove('X-Token');
+        Cookies.remove('X-User');
     }
     $("#token").text(_token);
     $("#user").text(_user);
