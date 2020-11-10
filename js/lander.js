01eab8f91d95d40f5b33ada73327bad0e2f6decc
Nacho <nachon49@gmail.com>
2020-11-10 19:14:11 -0300

    c26

--- js/lander.js
+++ js/lander.js
@@ -14,11 +14,18 @@ $("#getdata").click(()=>{
     let url_data='https://410iwdz3t5.execute-api.us-east-2.amazonaws.com/develop/rlz-data';
     $.ajax({
         url:url_data,
-        type:'GET',
+        type:'POST',
         contentType: 'application/json',
         headers: { 
             'x-user': Cookies.get('X-User'),
             'x-token': Cookies.get('X-Token')
+        },
+        data:{
+            "search_info": {
+                "method": "day_between",
+                "param1": "2020-10-20T00:00:00Z",
+                "param2": "2020-10-30T00:00:00Z"
+          }
         }
     }).done((data)=>{
         $("#datarlz").text(data);
